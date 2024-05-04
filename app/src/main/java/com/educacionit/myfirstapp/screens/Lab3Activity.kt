d386432b08c21e6609449ca3f1e23c2f738526a6
Jonathan Aranguri <arangurim@gmail.com>
2024-05-04 11:35:35 -0300

    feat: Lab 3

--- app/src/main/java/com/educacionit/myfirstapp/screens/Lab3Activity.kt
+++ app/src/main/java/com/educacionit/myfirstapp/screens/Lab3Activity.kt
@@ -1,6 +1,9 @@
 package com.educacionit.myfirstapp.screens
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +11,8 @@ import androidx.core.view.WindowInsetsCompat
 import com.educacionit.myfirstapp.R
 
 class Lab3Activity : AppCompatActivity() {
+    private lateinit var addButton: Button
+    private lateinit var resultTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +21,22 @@ class Lab3Activity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        addButton = findViewById(R.id.btnSumar)
+        resultTextView = findViewById(R.id.txtNumero)
+
+        addButton.setOnClickListener {
+            // Obtener el texto del textview
+            val originalTextString = resultTextView.text.toString()
+            // Convertir a Int
+            var originalTextInt = originalTextString.toInt()
+            // Incrementar el valor
+            originalTextInt++
+            // Convertir a string
+            val finalTextString = originalTextInt.toString()
+            // Reasignar el valor actualizado al textView
+            resultTextView.text = finalTextString
         }
     }
 }
