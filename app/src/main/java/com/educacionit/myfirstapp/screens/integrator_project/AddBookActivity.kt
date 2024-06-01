80a08e8f2550fd50d45fe1ce1947fef6d97bc004
Jonathan Aranguri <arangurim@gmail.com>
2024-06-01 12:45:04 -0300

    feat: IntegratorProject 3.2

--- app/src/main/java/com/educacionit/myfirstapp/screens/integrator_project/AddBookActivity.kt
+++ app/src/main/java/com/educacionit/myfirstapp/screens/integrator_project/AddBookActivity.kt
@@ -1,15 +1,52 @@
 package com.educacionit.myfirstapp.screens.integrator_project
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.educacionit.myfirstapp.R
+import com.educacionit.myfirstapp.screens.integrator_project.model.Book
 
 class AddBookActivity : AppCompatActivity() {
+    private lateinit var bookTitleEditText: EditText
+    private lateinit var bookAuthorEditText: EditText
+    private lateinit var saveBookButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_book)
+        bookTitleEditText = findViewById(R.id.title_edittext)
+        bookAuthorEditText = findViewById(R.id.author_edittext)
+        saveBookButton = findViewById(R.id.save_book_button)
+
+        configureViews()
+    }
+
+    private fun configureViews() {
+        saveBookButton.setOnClickListener {
+            saveBook()
+        }
+    }
+
+    private fun saveBook() {
+        val inputTitle = bookTitleEditText.text.toString()
+        val inputAuthor = bookAuthorEditText.text.toString()
+        if (inputTitle.isEmpty()) {
+            Toast.makeText(this, "Ingrese el titulo", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (inputAuthor.isEmpty()) {
+            Toast.makeText(this, "Ingrese el autor", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // TODO: Implement logic to send Book to previous screen
+        val newBook = Book(title = inputTitle, author = inputAuthor)
+        Toast.makeText(this, newBook.toString(), Toast.LENGTH_SHORT).show()
+        finish()
     }
 }
