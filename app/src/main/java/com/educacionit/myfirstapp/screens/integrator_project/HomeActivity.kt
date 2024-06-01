124bd9a91eea7c60f592daaa230e255340921220
Jonathan Aranguri <arangurim@gmail.com>
2024-06-01 12:24:00 -0300

    feat: IntegratorProject 3.1

--- app/src/main/java/com/educacionit/myfirstapp/screens/integrator_project/HomeActivity.kt
+++ app/src/main/java/com/educacionit/myfirstapp/screens/integrator_project/HomeActivity.kt
@@ -4,22 +4,53 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
 import com.educacionit.myfirstapp.R
+import com.educacionit.myfirstapp.screens.integrator_project.adapters.BooksAdapter
+import com.educacionit.myfirstapp.screens.integrator_project.model.Book
+import com.educacionit.myfirstapp.screens.integrator_project.model.BooksRepository
+import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var booksToolbar: Toolbar
+    private lateinit var booksRecyclerView: RecyclerView
+    private lateinit var booksAdapter: BooksAdapter
+    private lateinit var loadingView: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         booksToolbar = findViewById(R.id.books_toolbar)
-        configureActionBar()
+        booksRecyclerView = findViewById(R.id.books_recyclerview)
+        loadingView = findViewById(R.id.loading_view)
+
+        configureViews()
         getParamsFromIntent()
+    }
+
+    private fun configureViews() {
+        configureActionBar()
+        booksAdapter = BooksAdapter()
+        booksRecyclerView.adapter = booksAdapter
+        booksAdapter.setOnBookClickListener(object : BooksAdapter.OnBookClickListener {
+            override fun onBookClicked(book: Book) {
+                Toast.makeText(this@HomeActivity, book.title, Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lifecycleScope.launch {
+            loadingView.visibility = View.VISIBLE
+            booksAdapter.setBooks(BooksRepository.geBooks())
+            loadingView.visibility = View.GONE
+        }
     }
 
     private fun configureActionBar() {
---
+++ app/src/main/java/com/educacionit/myfirstapp/screens/integrator_project/model/Book.kt
@@ -0,0 +1,3 @@
+package com.educacionit.myfirstapp.screens.integrator_project.model
+
+data class Book(val title: String, val author: String)
